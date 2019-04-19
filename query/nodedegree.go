8a1490ad0b27c62b95c730e0628d414d83224674
brain.duan <brain.duan@analyticservice.net>
2019-04-19 11:09:36 +0800

    add node degree func

--- query/nodedegree.go
+++ query/nodedegree.go
@@ -18,10 +18,28 @@ func NodeDegree(param ext.ProcessFuncParam) (map[uint64]types.Val, error) {
 		destMap[uid] = 0
 	}
 	for _, edgePred := range param.ParamLabels {
-		err := doComputeNodeOutDegree(param.Context, param.SrcUids, edgePred, param.ReadTs, destMap)
+		err := doComputeNodeOutDegree(param.Context, param.SrcUids, edgePred, false, param.ReadTs, destMap)
 		if err != nil {
 			log.Printf("error when compute node outdegree %v", err)
 			return nil, err
+		}
+	}
+	// all schemas
+	schs, err := worker.GetSchemaOverNetwork(param.Context, &pb.SchemaRequest{})
+	if err != nil {
+		log.Printf("error when compute node outdegree %v", err)
+		return nil, err
+	}
+	// find reverse
+	for _, edgePred := range param.ParamLabels {
+		for _, sch := range schs {
+			if edgePred == sch.Predicate && sch.Reverse {
+				err := doComputeNodeOutDegree(param.Context, param.SrcUids, edgePred, true, param.ReadTs, destMap)
+				if err != nil {
+					log.Printf("error when compute node outdegree %v", err)
+					return nil, err
+				}
+			}
 		}
 	}
 	// trans to types.Val
@@ -39,12 +57,13 @@ func newList(data []uint64) *pb.List {
 	return &pb.List{Uids: data}
 }
 func doComputeNodeOutDegree(ctx context.Context, uidList *pb.List,
-	pred string, readTS uint64, destMap map[uint64]int64) error {
+	pred string, isReverse bool, readTS uint64, destMap map[uint64]int64) error {
 	//
 	taskQuery := &pb.Query{
 		Attr:    pred,
 		UidList: uidList,
 		DoCount: true,
+		Reverse: isReverse,
 		ReadTs:  readTS,
 	}
 	result, err := worker.ProcessTaskOverNetwork(ctx, taskQuery)
@@ -52,7 +71,11 @@ func doComputeNodeOutDegree(ctx context.Context, uidList *pb.List,
 		return x.Errorf("Error in NodeDegree { pred:%s ,msg: %v}", pred, err)
 	}
 	for i := 0; i < len(result.Counts); i++ {
-		destMap[uidList.Uids[i]] += int64(result.Counts[i])
+		if isReverse {
+			destMap[uidList.Uids[i]] -= int64(result.Counts[i])
+		} else {
+			destMap[uidList.Uids[i]] += int64(result.Counts[i])
+		}
 	}
 	return nil
 }
